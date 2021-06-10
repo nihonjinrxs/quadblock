ec20e88340b4e443aadb31a879caf9a4f7ddc0ba
Ryan B. Harvey <ryan.b.harvey@ted.com>
2021-06-10 00:50:38 -0500

    Lesson 15 extension: refactor play button into component

--- lib/tetris_web/live/game_live/game_over.ex
+++ lib/tetris_web/live/game_live/game_over.ex
@@ -6,8 +6,4 @@ defmodule TetrisWeb.GameLive.GameOver do
   def mount(_params, _session, socket) do
     {:ok, assign(socket, game: Map.get(socket.assigns, :game) || Game.new)}
   end
-
-  def handle_event("play", _, socket), do: {:noreply, play(socket)}
-
-  defp play(socket), do: push_redirect(socket, to: "/game/playing")
 end
---
+++ lib/tetris_web/live/game_live/play_button.ex
@@ -0,0 +1,15 @@
+defmodule TetrisWeb.GameLive.PlayButton do
+  use TetrisWeb, :live_component
+
+  def render(assigns) do
+    ~L"""
+    <button class="css-framework-class" phx-click="play" phx-target="<%= @myself %>">
+      <%= @title %>
+    </button>
+    """
+  end
+
+  def handle_event("play", _, socket), do: {:noreply, play(socket)}
+
+  defp play(socket), do: push_redirect(socket, to: "/game/playing")
+end
